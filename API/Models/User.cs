91253b5b16c0947e8f0c3968dd292b9b21ea5fb7
ivk <ivan.keynes@bbconsult.co.uk>
2022-10-30 20:04:40 +0600

    removed data for Identity

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -3,7 +3,8 @@ namespace API.models
     public class User
     {
         public int Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
