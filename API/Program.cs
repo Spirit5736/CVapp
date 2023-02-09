5dd170a240b299733a48e5a0ed94a9fcc1cc3916
ivk <ivan.keynes@bbconsult.co.uk>
2023-02-09 22:31:43 +0100

    Added authentication middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using API.models;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -30,7 +33,21 @@ builder.Services.AddDbContext<UserContext>(options =>
 });
 
 builder.Services.AddCors();
+
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                .UTF8.GetBytes(builder.Configuration["TokenKey"])),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+            };
+        }
+        );
 
 var app = builder.Build();
 
@@ -43,9 +60,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
-app.UseAuthentication();
+
 
 app.MapControllers();
 
