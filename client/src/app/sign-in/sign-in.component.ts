ea75ccfc420705c4cafaf0e25566ba05a0484af5
ivk <ivan.keynes@bbconsult.co.uk>
2023-04-06 01:11:49 +0200

    #862jk4yg8 Changing page after authorization

--- client/src/app/sign-in/sign-in.component.ts
+++ client/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from '../_services/account.service';
 
@@ -8,13 +8,17 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  
+  
   model: any ={};
   loggedIn = false;
   registerForm = false;
 
-  constructor(private accountService : AccountService, private router: Router) { }
+  constructor(private accountService : AccountService, private router: Router) {
+   }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login(){
@@ -22,13 +26,23 @@ export class SignInComponent implements OnInit {
       next: response => {
         console.log(response);
         this.loggedIn = true;
+        this.accountService.setButtonClicked();
+        this.router.navigate(['/cv']);
       },
       error: error => console.log(error)      
     });
   }
 
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe({
+      next : user => this.loggedIn = !!user,
+      error : error => console.log(error)
+    })
+  }
+
   logout(){
     this.loggedIn = false;
+    this.accountService.logout();
   }
 
   register() {
